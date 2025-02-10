eefc22290ccb9bddbece1f00e6135439131a82f4
Magnus Werner Ottesen <magnuswernerottesen@gmail.com>
2025-02-10 11:51:09 +0100

    add filter featuring

--- src/js/ui.js
+++ src/js/ui.js
@@ -41,102 +41,109 @@ class Ui {
   }
 
   // Rendering the books collection
-  static renderBooks() {
+  static renderBooks(filter = "all") {
     const bookList = document.querySelector(".books__list");
     bookList.innerHTML = "";
     const booksCollection = JSON.parse(
       localStorage.getItem("books-collection")
     );
-    booksCollection.forEach((book, index) => {
-      // Containers
-      const bookCard = document.createElement("li");
-      const bookDetailsContainer = document.createElement("div");
-      const bookToolsContainer = document.createElement("div");
+    const filteredCollection =
+      filter === "all"
+        ? booksCollection
+        : booksCollection.filter((book) => book.bookType === filter);
 
-      const titleContainer = document.createElement("div");
-      const authorContainer = document.createElement("div");
-      const publisherContainer = document.createElement("div");
-      const dateContainer = document.createElement("div");
+    if (filteredCollection) {
+      filteredCollection.forEach((book, index) => {
+        // Containers
+        const bookCard = document.createElement("li");
+        const bookDetailsContainer = document.createElement("div");
+        const bookToolsContainer = document.createElement("div");
 
-      const bookTypeContainer = document.createElement("div");
-      const pagesOrNarratorContainer = document.createElement("div");
-      const printTypeOrDurationContainer = document.createElement("div");
+        const titleContainer = document.createElement("div");
+        const authorContainer = document.createElement("div");
+        const publisherContainer = document.createElement("div");
+        const dateContainer = document.createElement("div");
 
-      //Header
-      const titleHeader = document.createElement("h3");
-      const authorHeader = document.createElement("h3");
-      const publisherHeader = document.createElement("h3");
-      const dateHeader = document.createElement("h3");
-      const bookTypeHeader = document.createElement("h3");
-      const pagesOrNarratorHeader = document.createElement("h3");
-      const printTypeOrDurationHeader = document.createElement("h3");
+        const bookTypeContainer = document.createElement("div");
+        const pagesOrNarratorContainer = document.createElement("div");
+        const printTypeOrDurationContainer = document.createElement("div");
 
-      const title = document.createElement("span");
-      const author = document.createElement("span");
-      const publisher = document.createElement("span");
-      const date = document.createElement("span");
-      const bookType = document.createElement("span");
-      const pagesOrNarrator = document.createElement("span");
-      const printTypeOrDuration = document.createElement("span");
+        //Header
+        const titleHeader = document.createElement("h3");
+        const authorHeader = document.createElement("h3");
+        const publisherHeader = document.createElement("h3");
+        const dateHeader = document.createElement("h3");
+        const bookTypeHeader = document.createElement("h3");
+        const pagesOrNarratorHeader = document.createElement("h3");
+        const printTypeOrDurationHeader = document.createElement("h3");
 
-      const deleteButton = document.createElement("button");
-      const editButton = document.createElement("button");
+        const title = document.createElement("span");
+        const author = document.createElement("span");
+        const publisher = document.createElement("span");
+        const date = document.createElement("span");
+        const bookType = document.createElement("span");
+        const pagesOrNarrator = document.createElement("span");
+        const printTypeOrDuration = document.createElement("span");
 
-      //Appending elements
-      bookList.append(bookCard);
-      bookCard.append(bookDetailsContainer, bookToolsContainer);
-      bookDetailsContainer.append(
-        titleContainer,
-        authorContainer,
-        publisherContainer,
-        dateContainer,
-        bookTypeContainer,
-        pagesOrNarratorContainer,
-        printTypeOrDurationContainer
-      );
-      titleContainer.append(titleHeader, title);
-      authorContainer.append(authorHeader, author);
-      publisherContainer.append(publisherHeader, publisher);
-      dateContainer.append(dateHeader, date);
-      bookTypeContainer.append(bookTypeHeader, bookType);
-      pagesOrNarratorContainer.append(pagesOrNarratorHeader, pagesOrNarrator);
-      printTypeOrDurationContainer.append(
-        printTypeOrDurationHeader,
-        printTypeOrDuration
-      );
-      bookToolsContainer.append(deleteButton, editButton);
+        const deleteButton = document.createElement("button");
+        const editButton = document.createElement("button");
 
-      //Adding data
-      titleHeader.textContent = "Title:";
-      authorHeader.textContent = "Author:";
-      publisherHeader.textContent = "Publisher:";
-      dateHeader.textContent = "Date:";
-      bookTypeHeader.textContent = "Book type:";
-      pagesOrNarratorHeader.textContent =
-        book.bookType === "printed-book" ? "Pages:" : "Narrator:";
-      printTypeOrDurationHeader.textContent =
-        book.bookType === "printed-book" ? "Print type:" : "Duration:";
+        //Appending elements
+        bookList.append(bookCard);
+        bookCard.append(bookDetailsContainer, bookToolsContainer);
+        bookDetailsContainer.append(
+          titleContainer,
+          authorContainer,
+          publisherContainer,
+          dateContainer,
+          bookTypeContainer,
+          pagesOrNarratorContainer,
+          printTypeOrDurationContainer
+        );
+        titleContainer.append(titleHeader, title);
+        authorContainer.append(authorHeader, author);
+        publisherContainer.append(publisherHeader, publisher);
+        dateContainer.append(dateHeader, date);
+        bookTypeContainer.append(bookTypeHeader, bookType);
+        pagesOrNarratorContainer.append(pagesOrNarratorHeader, pagesOrNarrator);
+        printTypeOrDurationContainer.append(
+          printTypeOrDurationHeader,
+          printTypeOrDuration
+        );
+        bookToolsContainer.append(deleteButton, editButton);
 
-      title.textContent = book.title;
-      author.textContent = book.author;
-      publisher.textContent = book.publisher;
-      date.textContent = book.date;
-      bookType.textContent = book.bookType;
-      pagesOrNarrator.textContent =
-        book.bookType === "printed-book" ? book.pages : book.narrator;
-      printTypeOrDuration.textContent =
-        book.bookType === "printed-book" ? book.printType : book.duration;
-      deleteButton.textContent = "Delete";
-      editButton.textContent = "Edit";
+        //Adding data
+        titleHeader.textContent = "Title:";
+        authorHeader.textContent = "Author:";
+        publisherHeader.textContent = "Publisher:";
+        dateHeader.textContent = "Date:";
+        bookTypeHeader.textContent = "Book type:";
+        pagesOrNarratorHeader.textContent =
+          book.bookType === "printed-book" ? "Pages:" : "Narrator:";
+        printTypeOrDurationHeader.textContent =
+          book.bookType === "printed-book" ? "Print type:" : "Duration:";
 
-      //Adding classes
-      bookList.classList.add("books__list");
-      bookCard.classList.add("book-item");
-      bookDetailsContainer.classList.add("book-item__details-container");
-      bookToolsContainer.classList.add("book-item__tools-container");
-      deleteButton.classList.add("book-item__delete-button");
-      editButton.classList.add("book-item__edit-button");
-    });
+        title.textContent = book.title;
+        author.textContent = book.author;
+        publisher.textContent = book.publisher;
+        date.textContent = book.date;
+        bookType.textContent = book.bookType;
+        pagesOrNarrator.textContent =
+          book.bookType === "printed-book" ? book.pages : book.narrator;
+        printTypeOrDuration.textContent =
+          book.bookType === "printed-book" ? book.printType : book.duration;
+        deleteButton.textContent = "Delete";
+        editButton.textContent = "Edit";
+
+        //Adding classes
+        bookList.classList.add("books__list");
+        bookCard.classList.add("book-item");
+        bookDetailsContainer.classList.add("book-item__details-container");
+        bookToolsContainer.classList.add("book-item__tools-container");
+        deleteButton.classList.add("book-item__delete-button");
+        editButton.classList.add("book-item__edit-button");
+      });
+    }
   }
 }
 export default Ui;
